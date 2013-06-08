db4903a1506903014fec7cfcfa7610c9c10c5a7a
Roop <roop@hakz.co>
2013-06-08 18:42:29 +0800

    Added controller

---
+++ app/controllers/BoardsController.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Hakz Project in Laravel
+ * @author roop <roop@hakz.co>
+ * @copyright 2013, Hakz Project, http://hakz.co/
+ * @package Discussion Board Controller
+ */
+
+class BoardsController extends BaseController {
+
+	/**
+	 * Calls a local function that cannot be served with Laravel's presets
+	 * @param string Function name
+	 * @param string Parameters
+	 */
+	public static function __callFunc($method, $params) {
+		$params = is_array($param) ? implode('\'', $params) : $params;
+		eval("self::$method('$params');");
+	}
+
+	/**
+	 * Discussion board index page
+	 * @link boards/index
+	 */
+	public function getIndex() {
+		$data = Boards::fetchBoards();
+
+		$content = View::make('boards.index', array('content' => $data));
+		return $this->layout->with(array('content' => $content));
+	}
+
+	/**
+	 * Fetches a specific board
+	 * @link boards/b/{board-slug}
+	 */
+	private static function fetchBoard($name) {
+		$data = Boards::fetchBoard($name);
+
+		$content = View::make('boards.board', $data);
+		echo View::make('hakz.scaffolding')->with(array('content' => $content));
+	}
+
+	/**
+	 * Fetches a specific thread
+	 * @link boards/t/{sha1-slug}
+	 */
+	private static function fetchThread($slug) {
+		$data = Boards::fetchThread($slug);
+
+		$content = View::make('boards.thread', $data);
+		echo View::make('hakz.scaffolding')->with(array('content' => $content));
+	}
+}
