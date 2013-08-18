ee3df8b6f98be13ecd27f80b19dda223e7cf84c8
Roop <roop@hakz.co>
2013-08-18 19:32:28 +0900

    DevNotes 180813:03

--- app/routes.php
+++ app/routes.php
@@ -14,32 +14,44 @@
 Route::group(array('prefix' => 'boards'), function () {
 
 	/**
+	 * Views a board
 	 * @link boards/b/{board-slug}
 	 */
 	Route::get('b/{name}', function($name) {
 		BoardsController::__callFunc('fetchBoard', $name);
-	})->where('name', '[a-z\-]+');
+	})->where('name', '[a-z0-9\-]+');
 
 	/**
+	 * Views a thread
 	 * @link boards/t/{sha1_hash}.html
 	 */
-	Route::get('t/{thread}.html', function($thread) {
-		BoardsController::__callFunc('fetchThread', $thread);
-	})->where('thread', '[a-z0-9]{40}');
+	Route::get('t/{post}.html', function($post) {
+		BoardsController::__callFunc('fetchThread', $post);
+	})->where('post', '[a-z0-9]{40}');
 
 	/**
-	 * @link boards/p/{board-slug} [AJAX]
+	 * Edits a specific post
+	 * @link boards/e/{sha1_hash}
 	 */
-	Route::get('p/{name}', function($name) {
-		BoardsController::__callFunc('newPost', $name);
-	})->where('name', '[a-z0-9A-Z\-]');
+	Route::post('e/{post}', array('before' => 'csrf', function($post) {
+		BoardsController::__call('editPost', $post);
+	}))->where('post', '[a-z\-]+');
 
 	/**
-	 * @link boards/e/{hash-slug} [AJAX]
+	 * Posts a new thread
+	 * @link boards/p/{board-slug}
 	 */
-	Route::get('e/{slug}', function($slug) {
-		BoardsController::__callFunc('alterPost', $slug);
-	})->where('thread', '[a-z0-9]{40}');
+	Route::post('p/{slug}', array('before' => 'csrf', function($slug) {
+		BoardsController::__callFunc('newThread', $slug);
+	}))->where('slug', '[a-z\-]+');
+
+	/**
+	 * Replies to a thread
+	 * @link boards/r/{sha1_hash}
+	 */
+	Route::post('r/{hash}', array('before' => 'csrf', function($hash) {
+		BoardsController::__callFunc('newReply', $hash);
+	}))->where('thread', '[a-z0-9]{40}');
 
 });
 
